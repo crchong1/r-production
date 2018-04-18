b239a9ad8cfa25c8635dbb88ff7f7cb02b180190
hiyoriyoshida <green.hiyori@gmail.com>
2018-04-17 20:36:33 -0400

    resolve merge conflicts in medicationRoutes

--- routes/medicationRoutes.js
+++ routes/medicationRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var medicationDB = require('../database/medicationDB.js');
 
 //medications page functions
@@ -184,129 +183,10 @@ var deleteAcuteMed = function (req, res) {
     }
   });
 };
-=======
-
-//functions for chronic Med problems
-var getAllChronicMed = function (req, res) {
-    console.log("getAllChronicMed called in routes")
-    var id = req.body.id;
-    medicationDB.getAllChronicMed(id, function (data, err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("chronic med routes data: " + data);
-        res.send({ data: data });
-      }
-    });
-  };
-  
-  
-  var submitNewChronicMed = function (req, res) {
-    console.log("submitNewChronicMed called in routes")
-    var id = req.body.id;
-    // check to see if there is already data in the table
-    medicationDB.getAllChronicMed(id, function (data, err) {
-      if (err) {
-        console.log(err);
-      } else {
-        // if there is no data in the table yet
-        if (data.length == 0) {
-          // we create a new entry here
-          medicationDB.putNewChronicMed(req.body.id, req.body.chronicMedName,
-            req.body.chronicMedDose, req.body.chronicMedTime, req.body.chronicMedRoute,
-            req.body.chronicMedStartDate, req.body.chronicMedEndDate,
-            req.body.chronicMedDiagnosis, function (data, err) {
-              if (err) {
-                console.log(err);
-              } else {
-                res.send({ data: data });
-              }
-            });
-        }
-        // if there is already data in the table so we need to update it
-        else {
-          // this function takes in existing data and updates it
-          medicationDB.putChronicMedEntry(req.body.id, req.body.chronicMedName,
-            req.body.chronicMedDose, req.body.chronicMedTime, req.body.chronicMedRoute,
-            req.body.chronicMedStartDate, req.body.chronicMedEndDate,
-            req.body.chronicMedDiagnosis, function (data, err) {
-              if (err) {
-                console.log("error")
-              }
-              else if (data) {
-                res.send({
-                  data: data
-                });
-              }
-            });
-        }
-      }
-    });
-  };
-  
-  //functions for acute Med problems
-  var getAllAcuteMed = function (req, res) {
-    console.log("getAllAcuteMed called in routes")
-    var id = req.body.id;
-    medicationDB.getAllAcuteMed(id, function (data, err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("acute med routes data: " + data);
-        res.send({ data: data });
-      }
-    });
-  };
-  
-  
-  var submitNewAcuteMed = function (req, res) {
-    console.log("submitNewAcuteMed called in routes")
-    var id = req.body.id;
-    // check to see if there is already data in the table
-    medicationDB.getAllAcuteMed(id, function (data, err) {
-      if (err) {
-        console.log(err);
-      } else {
-        // if there is no data in the table yet
-        if (data.length == 0) {
-          // we create a new entry here
-          medicationDB.putNewAcuteMed(req.body.id, req.body.acuteMedName,
-            req.body.acuteMedDose, req.body.acuteMedTime, req.body.acuteMedRoute,
-            req.body.acuteMedStartDate, req.body.acuteMedEndDate,
-            req.body.acuteMedDiagnosis, function (data, err) {
-              if (err) {
-                console.log(err);
-              } else {
-                res.send({ data: data });
-              }
-            });
-        }
-        // if there is already data in the table so we need to update it
-        else {
-          // this function takes in existing data and updates it
-          medicationDB.putAcuteMedEntry(req.body.id, req.body.acuteMedName,
-            req.body.acuteMedDose, req.body.acuteMedTime, req.body.acuteMedRoute,
-            req.body.acuteMedStartDate, req.body.acuteMedEndDate,
-            req.body.acuteMedDiagnosis, function (data, err) {
-              if (err) {
-                console.log("error")
-              }
-              else if (data) {
-                res.send({
-                  data: data
-                });
-              }
-            });
-        }
-      }
-    });
-  };
->>>>>>> e81b08ff875b634ca1de8884cb05e9ef844452ed
   
 
   // this method handles the get_main request from app.js and reroutes it to the getMain function above
 var medicationRoutes = { 
-<<<<<<< HEAD
   //medications functions
   get_all_chronic_med: getAllChronicMed,
   submit_chronic_med: submitNewChronicMed,
@@ -317,12 +197,6 @@ var medicationRoutes = {
   submit_acute_med: submitNewAcuteMed,
   edit_acute_med: editAcuteMed,
   delete_acute_med: deleteAcuteMed
-=======
-    get_all_chronic_med: getAllChronicMed,
-    submit_chronic_med: submitNewChronicMed,
-    get_all_acute_med: getAllAcuteMed,
-    submit_acute_med: submitNewAcuteMed
->>>>>>> e81b08ff875b634ca1de8884cb05e9ef844452ed
   };
   
   module.exports = medicationRoutes;
